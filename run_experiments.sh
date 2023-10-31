7b174054581cb24e04828b018c05e55e587c5027
Hossein Haeri <46456383+hossein-haeri@users.noreply.github.com>
2023-10-31 11:47:40 -0400

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd maddpg/experiments/
-# python train_v0.py --exp-name Tribal_01 &   # The & at the end will run the script in the background
-# python train_v0.py --exp-name Tribal_02 &
-# python train_v0.py --exp-name Tribal_03 &
-# python train_v0.py --exp-name Tribal_04 &
-# python train_v0.py --exp-name Tribal_05 &
+python train_v0.py --exp-name Tribal_01 &   # The & at the end will run the script in the background
+python train_v0.py --exp-name Tribal_02 &
+python train_v0.py --exp-name Tribal_03 &
+python train_v0.py --exp-name Tribal_04 &
+python train_v0.py --exp-name Tribal_05 &
 # python train_v0.py --exp-name Tribal_06 &
 # python train_v0.py --exp-name Tribal_07 &
 # python train_v0.py --exp-name Tribal_08 &
@@ -12,10 +12,10 @@ cd maddpg/experiments/
 # python train_v0.py --exp-name Tribal_10 &
 
 
-python train_v0.py --exp-name Tribal_02 --restore --load-dir ./saved_policy/Tribal_02/&
-python train_v0.py --exp-name Tribal_03 --restore --load-dir ./saved_policy/Tribal_03/&
-python train_v0.py --exp-name Tribal_04 --restore --load-dir ./saved_policy/Tribal_04/&
-python train_v0.py --exp-name Tribal_05 --restore --load-dir ./saved_policy/Tribal_05/&
+# python train_v0.py --exp-name Tribal_02 --restore --load-dir ./saved_policy/Tribal_02/&
+# python train_v0.py --exp-name Tribal_03 --restore --load-dir ./saved_policy/Tribal_03/&
+# python train_v0.py --exp-name Tribal_04 --restore --load-dir ./saved_policy/Tribal_04/&
+# python train_v0.py --exp-name Tribal_05 --restore --load-dir ./saved_policy/Tribal_05/&
 
 # python train_v0.py --exp-name Tribal_01 --restore --load-dir ./saved_policy/Tribal_01/
 
