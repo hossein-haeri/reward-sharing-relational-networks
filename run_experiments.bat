2fb4de036797cdb21016d15eab1065e77e99cf88
Hossein Haeri <46456383+hossein-haeri@users.noreply.github.com>
2023-11-01 18:58:25 -0400

    update

--- run_experiments.bat
+++ run_experiments.bat
@@ -2,16 +2,14 @@
 cd C:\Users\Hossein_Haeri\reward-sharing-relational-networks\venv\Scripts
 call activate
 cd ..\..\maddpg\experiments
-start python train_v0.py --exp-name fully-connected_01
-@REM start python train_v0.py --exp-name fully-connected_02
-@REM start python train_v0.py --exp-name fully-connected_03
-@REM start python train_v0.py --exp-name fully-connected_04
-@REM start python train_v0.py --exp-name fully-connected_05
-@REM start python train_v0.py --exp-name fully-connected_06
-@REM start python train_v0.py --exp-name fully-connected_07
-@REM start python train_v0.py --exp-name fully-connected_08
-@REM start python train_v0.py --exp-name fully-connected_09
-@REM start python train_v0.py --exp-name fully-connected_10
-@REM start python train_v0.py --exp-name fully-connected_11
-@REM start python train_v0.py --exp-name fully-connected_12
+start python train_v0.py --exp-name fully-connected_bounded_01
+start python train_v0.py --exp-name fully-connected_bounded_02
+start python train_v0.py --exp-name fully-connected_bounded_03
+start python train_v0.py --exp-name fully-connected_bounded_04
+start python train_v0.py --exp-name fully-connected_bounded_05
+start python train_v0.py --exp-name fully-connected_bounded_06
+start python train_v0.py --exp-name fully-connected_bounded_07
+start python train_v0.py --exp-name fully-connected_bounded_08
+start python train_v0.py --exp-name fully-connected_bounded_09
+start python train_v0.py --exp-name fully-connected_bounded_10
 cmd /k
