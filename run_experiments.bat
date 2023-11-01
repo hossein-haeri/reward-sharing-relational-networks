1f17d6199e0a4c053f9be90b19637f7edd84a5bd
Hossein Haeri <46456383+hossein-haeri@users.noreply.github.com>
2023-11-01 13:52:03 -0400

    Update run_experiments.bat

--- run_experiments.bat
+++ run_experiments.bat
@@ -2,14 +2,14 @@
 cd C:\Users\Hossein_Haeri\reward-sharing-relational-networks\venv\Scripts
 call activate
 cd ..\..\maddpg\experiments
-start python train_v0.py --exp-name athoritarian_01
-start python train_v0.py --exp-name athoritarian_02
-start python train_v0.py --exp-name athoritarian_03
-start python train_v0.py --exp-name athoritarian_04
-start python train_v0.py --exp-name athoritarian_05
-start python train_v0.py --exp-name athoritarian_06
-start python train_v0.py --exp-name athoritarian_07
-start python train_v0.py --exp-name athoritarian_08
-start python train_v0.py --exp-name athoritarian_09
-start python train_v0.py --exp-name athoritarian_10
+start python train_v0.py --exp-name fully-connected_01
+start python train_v0.py --exp-name fully-connected_02
+start python train_v0.py --exp-name fully-connected_03
+start python train_v0.py --exp-name fully-connected_04
+start python train_v0.py --exp-name fully-connected_05
+start python train_v0.py --exp-name fully-connected_06
+start python train_v0.py --exp-name fully-connected_07
+start python train_v0.py --exp-name fully-connected_08
+start python train_v0.py --exp-name fully-connected_09
+start python train_v0.py --exp-name fully-connected_10
 cmd /k
