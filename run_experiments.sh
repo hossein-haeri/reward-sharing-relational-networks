54b4ba97173c5644b9bef2624d038391e8ca99a6
Hossein Haeri <46456383+hossein-haeri@users.noreply.github.com>
2023-11-01 14:00:55 -0400

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 cd maddpg/experiments/
-python python train_v0.py --exp-name fully-connected_01
-python python train_v0.py --exp-name fully-connected_02
-python python train_v0.py --exp-name fully-connected_03
-python python train_v0.py --exp-name fully-connected_04
-python python train_v0.py --exp-name fully-connected_05
-python python train_v0.py --exp-name fully-connected_06
-python python train_v0.py --exp-name fully-connected_07
-python python train_v0.py --exp-name fully-connected_08
-python python train_v0.py --exp-name fully-connected_09
-python python train_v0.py --exp-name fully-connected_10
-python python train_v0.py --exp-name fully-connected_11
-python python train_v0.py --exp-name fully-connected_12
+python train_v0.py --exp-name fully-connected_01
+python train_v0.py --exp-name fully-connected_02
+python train_v0.py --exp-name fully-connected_03
+python train_v0.py --exp-name fully-connected_04
+python train_v0.py --exp-name fully-connected_05
+python train_v0.py --exp-name fully-connected_06
+python train_v0.py --exp-name fully-connected_07
+python train_v0.py --exp-name fully-connected_08
+python train_v0.py --exp-name fully-connected_09
+python train_v0.py --exp-name fully-connected_10
+python train_v0.py --exp-name fully-connected_11
+python train_v0.py --exp-name fully-connected_12
 
 wait  # This will wait for all background processes to complete
