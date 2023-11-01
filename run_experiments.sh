4bc7914373add6e33fd437073ca5eb4176487ab9
Hossein Haeri <46456383+hossein-haeri@users.noreply.github.com>
2023-11-01 13:59:43 -0400

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 cd maddpg/experiments/
-python train_v0.py --exp-name Tribal_01 &   # The & at the end will run the script in the background
-python train_v0.py --exp-name Tribal_02 &
-python train_v0.py --exp-name Tribal_03 &
-python train_v0.py --exp-name Tribal_04 &
-python train_v0.py --exp-name Tribal_05 &
-# python train_v0.py --exp-name Tribal_06 &
-# python train_v0.py --exp-name Tribal_07 &
-# python train_v0.py --exp-name Tribal_08 &
-# python train_v0.py --exp-name Tribal_09 &
-# python train_v0.py --exp-name Tribal_10 &
-
-
-# python train_v0.py --exp-name Tribal_02 --restore --load-dir ./saved_policy/Tribal_02/&
-# python train_v0.py --exp-name Tribal_03 --restore --load-dir ./saved_policy/Tribal_03/&
-# python train_v0.py --exp-name Tribal_04 --restore --load-dir ./saved_policy/Tribal_04/&
-# python train_v0.py --exp-name Tribal_05 --restore --load-dir ./saved_policy/Tribal_05/&
-
-# python train_v0.py --exp-name Tribal_01 --restore --load-dir ./saved_policy/Tribal_01/
+python python train_v0.py --exp-name fully-connected_01
+python python train_v0.py --exp-name fully-connected_02
+python python train_v0.py --exp-name fully-connected_03
+python python train_v0.py --exp-name fully-connected_04
+python python train_v0.py --exp-name fully-connected_05
+python python train_v0.py --exp-name fully-connected_06
+python python train_v0.py --exp-name fully-connected_07
+python python train_v0.py --exp-name fully-connected_08
+python python train_v0.py --exp-name fully-connected_09
+python python train_v0.py --exp-name fully-connected_10
+python python train_v0.py --exp-name fully-connected_11
+python python train_v0.py --exp-name fully-connected_12
 
 wait  # This will wait for all background processes to complete
