7e2e3b0421b73fd12c2d51d81ca53022ac37f625
Hossein Haeri <46456383+hossein-haeri@users.noreply.github.com>
2023-11-02 22:41:28 -0400

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -5,19 +5,16 @@ cd maddpg/experiments/
 num_runs=3
 num_episodes=250000
 
-for i in {1..$num_runs}
-do
-python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WSM --network fully-connected --num-episodes $num_episodes --exp-name $i &
+for i in $(seq 1 $num_runs); do
+    python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WSM --network fully-connected --num-episodes $num_episodes --exp-name $i  &
 done
 
-for i in {1..$num_runs}
-do
-python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WPM --network fully-connected --num-episodes $num_episodes --exp-name $i &
+for i in $(seq 1 $num_runs); do
+    python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WPM --network fully-connected --num-episodes $num_episodes --exp-name $i  &
 done
 
-for i in {1..$num_runs}
-do
-python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type MinMax --network fully-connected --num-episodes $num_episodes --exp-name $i &
+for i in $(seq 1 $num_runs); do
+    python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type MinMax --network fully-connected --num-episodes $num_episodes --exp-name $i  &
 done
 
-wait  # This will wait for all background processes to complete
+wait
