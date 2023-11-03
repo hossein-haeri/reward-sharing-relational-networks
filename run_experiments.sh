96a7f58d83c16232c4d1045331ebbdc646a67611
Hossein Haeri <46456383+hossein-haeri@users.noreply.github.com>
2023-11-02 22:26:04 -0400

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 cd maddpg/experiments/
 
-python train_two_agent.py --exp-name two-agent_wsm_fully-connected_slow_01 &
-python train_two_agent.py --exp-name two-agent_wsm_fully-connected_slow_02 &
-python train_two_agent.py --exp-name two-agent_wsm_fully-connected_slow_03 &
-python train_two_agent.py --exp-name two-agent_wsm_fully-connected_slow_04 &
-python train_two_agent.py --exp-name two-agent_wsm_fully-connected_slow_05 &
+
+num_runs=3
+num_episodes=250000
+
+for i in {1..$num_runs}
+do
+python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WSM --network fully-connected --num-episdoes $num_episodes --exp-name $i &
+done
+
+for i in {1..$num_runs}
+do
+python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WPM --network fully-connected --num-episdoes $num_episodes --exp-name $i &
+done
+
+for i in {1..$num_runs}
+do
+python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type MinMax --network fully-connected --num-episdoes $num_episodes --exp-name $i &
+done
 
 wait  # This will wait for all background processes to complete
