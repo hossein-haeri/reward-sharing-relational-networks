91ff602114d0818fee693529c557bb5486336532
Hossein Haeri <46456383+hossein-haeri@users.noreply.github.com>
2023-11-02 22:27:17 -0400

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -7,17 +7,17 @@ num_episodes=250000
 
 for i in {1..$num_runs}
 do
-python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WSM --network fully-connected --num-episdoes $num_episodes --exp-name $i &
+python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WSM --network fully-connected --num-episodes $num_episodes --exp-name $i &
 done
 
 for i in {1..$num_runs}
 do
-python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WPM --network fully-connected --num-episdoes $num_episodes --exp-name $i &
+python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type WPM --network fully-connected --num-episodes $num_episodes --exp-name $i &
 done
 
 for i in {1..$num_runs}
 do
-python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type MinMax --network fully-connected --num-episdoes $num_episodes --exp-name $i &
+python train_v3.py --num-agents 2 --num-landmarks 2 --rsrn-type MinMax --network fully-connected --num-episodes $num_episodes --exp-name $i &
 done
 
 wait  # This will wait for all background processes to complete
